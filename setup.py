0d80230e0b5c0808697d205579deb564be7c23eb
Founder Nur1Labs <11433397+SoraKohaku@users.noreply.github.com>
2024-02-18 13:56:52 +0700

    Update setup.py
    update setup

--- setup.py
+++ setup.py
@@ -11,19 +11,19 @@ with open(os.path.join(here, 'README.md')) as f:
 
 requires = []
 
-setup(name='python-bitcoinlib',
+setup(name='python-mubdilib',
       version=__version__,
-      description='The Swiss Army Knife of the Bitcoin protocol.',
+      description='The Black Box of the MuBdI protocol.',
       long_description=README,
       long_description_content_type='text/markdown',
       classifiers=[
           "Programming Language :: Python",
           "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
       ],
-      url='https://github.com/petertodd/python-bitcoinlib',
-      keywords='bitcoin',
+      url='https://github.com/nur1labs/python-mubdilib',
+      keywords='mubdi',
       packages=find_packages(),
       zip_safe=False,
       install_requires=requires,
-      test_suite="bitcoin.tests"
+      test_suite="mubdi.tests"
      )
